18bb7c0548c6f82b51c5ad98406ef3ac073d1e33
thaoklg8 <you@example.comthaovtt19406@st.uel.edu.vn git config --global user.name thaoklg8git config --global user.pass 97452684Aa@git config --global user.pass 97452684Aa@ git config --global user.email you@example.comthaovtt19406@st.uel.edu.vn>
2021-11-04 14:06:43 +0700

    test

---
+++ SpringMVC/src/main/java/com/checongbinh/controller/TrangChuController.java
@@ -0,0 +1,18 @@
+package com.checongbinh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TrangChuController {
+	@RequestMapping("/")
+	public String ViewTrangChu() {
+		return "trangchu";
+	}
+	@RequestMapping("/login")
+	@ResponseBody
+	public String Login() {
+		return "Login";
+	}
+}
