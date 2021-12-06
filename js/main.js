e6e9189c03d385e7ff5bb55045aa4f2e2c93d822
thaoklg8 <you@example.comthaovtt19406@st.uel.edu.vn git config --global user.name thaoklg8git config --global user.pass 97452684Aa@git config --global user.pass 97452684Aa@ git config --global user.email you@example.comthaovtt19406@st.uel.edu.vn>
2021-12-07 00:41:05 +0700

    add json file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// check();
+
 function hiddenMenu() {
     var hidden = document.getElementById('page__menu--right');
     if (hidden.style.display === 'none') {
@@ -17,18 +19,18 @@ $(function() {
     $("#page__partner--slide").load("/html/slide.html");
 });
 
-function chuyenTrang(){
-    window.location="/html/home_index.html";
+function chuyenTrang() {
+    window.location = "/html/home_index.html";
 };
 
-function checkRegisEmail(){
+function checkRegisEmail() {
     var data = new Array();
     data[0] = document.getElementById('name').value;
     data[1] = document.getElementById('surname').value;
     data[2] = document.getElementById('Email').value;
     data[3] = document.getElementById('pass').value;
     data[4] = document.getElementById('repass').value;
-    
+
     var myerror = new Array();
     myerror[0] = "Bạn chưa nhập Tên";
     myerror[1] = "Bạn chưa nhập Họ";
@@ -37,38 +39,37 @@ function checkRegisEmail(){
     myerror[4] = "Bạn chưa xác nhận lại mật khẩu";
 
     var nearby = new Array("notname", "notsurname", "notemail", "notpass", "notrepass");
-    
+
     for (i in data) {
         var error = myerror[i];
         var div = nearby[i];
-        if (data[i]=="") {
+        if (data[i] == "") {
             document.getElementById(div).innerHTML = error;
-        }else{
+        } else {
             document.getElementById(div).innerHTML = "";
         }
     }
-    if ((i=4)&& (data[i]==data[i-1])){
+    if ((i = 4) && (data[i] == data[i - 1])) {
         document.getElementById('notrepass').innerHTML = "<span style='color:black'>Mật khẩu đã khớp</span>";
-    }else {
+    } else {
         document.getElementById('notrepass').innerHTML = "Mật khẩu chưa khớp";
     }
 
-    if ( document.getElementById('accept').checked ==true){
-        document.getElementById('notaccept').innerHTML ="<span style='color:black'>Bạn đã chấp nhận điều khoản sử dụng của công ty</span>";
-        }
-        else{
-            document.getElementById('notaccept').innerHTML ="Bạn chưa chấp nhận điều khoản sử dụng của công ty";
-        } 
+    if (document.getElementById('accept').checked == true) {
+        document.getElementById('notaccept').innerHTML = "<span style='color:black'>Bạn đã chấp nhận điều khoản sử dụng của công ty</span>";
+    } else {
+        document.getElementById('notaccept').innerHTML = "Bạn chưa chấp nhận điều khoản sử dụng của công ty";
+    }
 }
 
-function checkRegisPhone(){
+function checkRegisPhone() {
     var data = new Array();
     data[0] = document.getElementById('name').value;
     data[1] = document.getElementById('surname').value;
     data[2] = document.getElementById('Phone').value;
     data[3] = document.getElementById('pass').value;
     data[4] = document.getElementById('repass').value;
-    
+
     var myerror = new Array();
     myerror[0] = "Bạn chưa nhập Tên";
     myerror[1] = "Bạn chưa nhập Họ";
@@ -77,38 +78,37 @@ function checkRegisPhone(){
     myerror[4] = "Bạn chưa xác nhận lại mật khẩu";
 
     var nearby = new Array("notname", "notsurname", "notphone", "notpass", "notrepass");
-    
+
     for (i in data) {
         var error = myerror[i];
         var div = nearby[i];
-        if (data[i]=="") {
+        if (data[i] == "") {
             document.getElementById(div).innerHTML = error;
-        }else{
+        } else {
             document.getElementById(div).innerHTML = "";
         }
     }
-    if ((i=4)&& (data[i]==data[i-1])){
+    if ((i = 4) && (data[i] == data[i - 1])) {
         document.getElementById('notrepass').innerHTML = "<span style='color:black'>Mật khẩu đã khớp</span>";
-    }else {
+    } else {
         document.getElementById('notrepass').innerHTML = "Mật khẩu chưa khớp";
     }
 
-    if ( document.getElementById('accept').checked ==true){
-        document.getElementById('notaccept').innerHTML ="<span style='color:black'>Bạn đã chấp nhận điều khoản sử dụng của công ty</span>";
-        }
-        else{
-            document.getElementById('notaccept').innerHTML ="Bạn chưa chấp nhận điều khoản sử dụng của công ty";
-        }
+    if (document.getElementById('accept').checked == true) {
+        document.getElementById('notaccept').innerHTML = "<span style='color:black'>Bạn đã chấp nhận điều khoản sử dụng của công ty</span>";
+    } else {
+        document.getElementById('notaccept').innerHTML = "Bạn chưa chấp nhận điều khoản sử dụng của công ty";
+    }
 }
 
-function checkRegisEmail(){
+function checkRegisEmail() {
     var data = new Array();
     data[0] = document.getElementById('name').value;
     data[1] = document.getElementById('surname').value;
     data[2] = document.getElementById('Email').value;
     data[3] = document.getElementById('pass').value;
     data[4] = document.getElementById('repass').value;
-    
+
     var myerror = new Array();
     myerror[0] = "Bạn chưa nhập Tên";
     myerror[1] = "Bạn chưa nhập Họ";
@@ -117,74 +117,101 @@ function checkRegisEmail(){
     myerror[4] = "Bạn chưa xác nhận lại mật khẩu";
 
     var nearby = new Array("notname", "notsurname", "notemail", "notpass", "notrepass");
-    
+
     for (i in data) {
         var error = myerror[i];
         var div = nearby[i];
-        if (data[i]=="") {
+        if (data[i] == "") {
             document.getElementById(div).innerHTML = error;
-        }else{
+        } else {
             document.getElementById(div).innerHTML = "";
         }
     }
-    if ((i=4)&& (data[i]==data[i-1])){
+    if ((i = 4) && (data[i] == data[i - 1])) {
         document.getElementById('notrepass').innerHTML = "<span style='color:black'>Mật khẩu đã khớp</span>";
-    }else {
+    } else {
         document.getElementById('notrepass').innerHTML = "Mật khẩu chưa khớp";
     }
 
-    if ( document.getElementById('accept').checked ==true){
-        document.getElementById('notaccept').innerHTML ="<span style='color:black'>Bạn đã chấp nhận điều khoản sử dụng của công ty</span>";
-        }
-        else{
-            document.getElementById('notaccept').innerHTML ="Bạn chưa chấp nhận điều khoản sử dụng của công ty";
-        } 
+    if (document.getElementById('accept').checked == true) {
+        document.getElementById('notaccept').innerHTML = "<span style='color:black'>Bạn đã chấp nhận điều khoản sử dụng của công ty</span>";
+    } else {
+        document.getElementById('notaccept').innerHTML = "Bạn chưa chấp nhận điều khoản sử dụng của công ty";
+    }
 }
 
-function checkSignEmail(){
+function checkSignEmail() {
     var data = new Array();
     data[0] = document.getElementById('Email').value;
     data[1] = document.getElementById('pass').value;
-    
+
     var myerror = new Array();
     myerror[0] = "Bạn chưa nhập Email";
     myerror[1] = "Bạn chưa nhập Mật khẩu";
-    
+
     var nearby = new Array("notemail", "notpass");
-    
+
     for (i in data) {
         var error = myerror[i];
         var div = nearby[i];
-        if (data[i]=="") {
+        if (data[i] == "") {
             document.getElementById(div).innerHTML = error;
-        }else{
+        } else {
             document.getElementById(div).innerHTML = "";
         }
     }
 }
 
-function checkSignPhone(){
+function checkSignPhone() {
     var data = new Array();
     data[0] = document.getElementById('Phone').value;
     data[1] = document.getElementById('pass').value;
-    
+
     var myerror = new Array();
     myerror[0] = "Bạn chưa nhập Số di động";
     myerror[1] = "Bạn chưa nhập Mật khẩu";
-    
+
     var nearby = new Array("notphone", "notpass");
-    
+
     for (i in data) {
         var error = myerror[i];
         var div = nearby[i];
-        if (data[i]=="") {
+        if (data[i] == "") {
             document.getElementById(div).innerHTML = error;
-        }else{
+        } else {
             document.getElementById(div).innerHTML = "";
         }
     }
 }
 
-
-
-
+function check() {
+    app.use(express.static('public'));
+}
+/*hiện thị inforTours*/
+function loadInforTours(n) {
+    fetch("../json/inforTours.json")
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error("HTTP error, status: " + response.status)
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            if (n === 0) {
+                n = data.inforTours.length;
+            }
+            for (let i = 0; i < n; i++) {
+                p = data.inforTours[i];
+                var left = document.createElement("div");
+                left.className = 'tour--item';
+                left.innerHTML = '<div class="tour--item-pic"><img src="' + p.img + '" alt=""></div>' +
+                    '<div class="tour--item--content"><div class="middle--item-content flexrow"><div class="tour--item--content--left">' + '<p class="title text-center">' + p.title + '</p>' +
+                    '<p class="title_province text-center"><i class="fas fa-map-pin" style="color: brown;"></i> ' + p.province + '</p>' +
+                    '</div><div class="tour--item--content--right price">' + p.price + ' </div></div>' +
+                    '<p class="title-script">' + p.script + '</p>' + '</div>' + '<div class="btn-detail btn-medium btn"><a href="' + p.detail + '">DETAILS</a></div>';
+                document.getElementById("jsonData").appendChild(left);
+            }
+        }).catch(function(error) {
+            alert("Error:" + error.message);
+        })
+}
